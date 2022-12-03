de7438c24a5fa2d851ee3ea31529da1539446623
fwcd <fwcdmail@gmail.com>
2022-12-03 13:11:51 +0100

    Read day 3 input

--- day03/src/main.zig
+++ day03/src/main.zig
@@ -1,11 +1,21 @@
 const std = @import("std");
 
 pub fn main() !void {
+    // Set up stdout
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Hello world\n", .{});
+    // Read input
+    var file = try std.fs.cwd().openFile("resources/demo.txt", .{});
+    defer file.close();
+    var br = std.io.bufferedReader(file.reader());
+    var input = br.reader();
 
-    try bw.flush(); // don't forget to flush!
+    var buf: [1024]u8 = undefined;
+    while (try input.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try stdout.print("Line: {s} of len {d}\n", .{line, line.len});
+    }
+
+    try bw.flush();
 }
