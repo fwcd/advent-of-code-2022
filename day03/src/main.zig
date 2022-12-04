390f4ac583389d342ad4249f8897c47b19626178
fwcd <fwcdmail@gmail.com>
2022-12-04 22:50:16 +0100

    Implement day 3 part 1 with bit sets

--- day03/src/main.zig
+++ day03/src/main.zig
@@ -8,19 +8,34 @@ pub fn charToIndex(c: u8) usize {
     }
 }
 
+pub fn indexToChar(i: usize) u8 {
+    if (i >= 26) {
+        return @truncate(u8, i - 26) + 'A';
+    } else {
+        return @truncate(u8, i + 'a');
+    }
+}
+
 pub fn score(c: u8) u32 {
     return @truncate(u32, charToIndex(c)) + 1;
 }
 
-pub fn findCommonItem(s: []u8) u8 {
-    var freqs = std.mem.zeroes([52]u8);
-    for (s[0..(s.len / 2)]) |c| {
-        freqs[charToIndex(c)] += 1;
+pub fn toSet(s: []u8) u64 {
+    var set: u64 = 0;
+    for (s) |c| {
+        set |= @as(u64, 1) << @intCast(u6, charToIndex(c));
     }
-    for (s[(s.len / 2)..]) |c| {
-        if (freqs[charToIndex(c)] > 0) {
-            return c;
+    return set;
+}
+
+pub fn firstChar(set: u64) u8 {
+    var value = set;
+    var i: u32 = 0;
+    while (i < 64) : (i += 1) {
+        if ((value & 1) == 1) {
+            return indexToChar(i);
         }
+        value = value >> 1;
     }
     return 0;
 }
@@ -37,14 +52,15 @@ pub fn main() !void {
     var br = std.io.bufferedReader(file.reader());
     var input = br.reader();
 
+    // Solve the problem
     var buf: [1024]u8 = undefined;
     var part1: u32 = 0;
     while (try input.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        const c = findCommonItem(line);
-        part1 += score(c);
+        const left = toSet(line[0..(line.len / 2)]);
+        const right = toSet(line[(line.len / 2)..]);
+        part1 += score(firstChar(left & right));
     }
 
     try stdout.print("Part 1: {d}\n", .{part1});
-
     try bw.flush();
 }
