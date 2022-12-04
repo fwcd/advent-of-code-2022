5fad7cba441e4d8164afb846aaecb0fabd0700c6
fwcd <fwcdmail@gmail.com>
2022-12-04 23:15:33 +0100

    Implement day 3 part 2

--- day03/src/main.zig
+++ day03/src/main.zig
@@ -1,11 +1,15 @@
 const std = @import("std");
 
+const group_size = 3;
+const full_set = @bitCast(u64, @as(i64, -1));
+
 pub fn charToIndex(c: u8) usize {
     if (c >= 'a' and c <= 'z') {
         return @as(usize, c - 'a');
-    } else {
+    } else if (c >= 'A' and c <= 'Z') {
         return @as(usize, c - 'A') + 26;
     }
+    @panic("Invalid char!");
 }
 
 pub fn indexToChar(i: usize) u8 {
@@ -54,13 +58,27 @@ pub fn main() !void {
 
     // Solve the problem
     var buf: [1024]u8 = undefined;
+    var group_set: u64 = full_set;
     var part1: u32 = 0;
+    var part2: u32 = 0;
+    var i: usize = 0;
+
     while (try input.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         const left = toSet(line[0..(line.len / 2)]);
         const right = toSet(line[(line.len / 2)..]);
+
         part1 += score(firstChar(left & right));
+        group_set &= left | right;
+
+        if (i % group_size == group_size - 1) {
+            part2 += score(firstChar(group_set));
+            group_set = full_set;
+        }
+
+        i += 1;
     }
 
     try stdout.print("Part 1: {d}\n", .{part1});
+    try stdout.print("Part 2: {d}\n", .{part2});
     try bw.flush();
 }
