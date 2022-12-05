3cd8f85e9cbc38bce86dedc3405a7c4c3f34a1a0
fwcd <fwcdmail@gmail.com>
2022-12-05 15:30:14 +0100

    Implement day 5 part 2

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -28,19 +28,23 @@
         insts (parse-insts raw-insts)]
     [stacks insts]))
 
-(defn perform-inst [stacks [n from to]]
+(defn perform-inst [order stacks [n from to]]
   (let [i (- from 1)
         j (- to 1)]
     (reduce (fn [x f] (f x)) stacks
-      [(fn [s] (update s j (fn [stack] (concat (reverse (take n (nth stacks i))) stack))))
+      [(fn [s] (update s j (fn [stack] (concat (order (take n (nth stacks i))) stack))))
        (fn [s] (update s i (fn [stack] (drop n stack))))])))
 
-(defn perform-insts [stacks insts]
-  (reduce perform-inst stacks insts))
+(defn perform-insts [order stacks insts]
+  (reduce (partial perform-inst order) stacks insts))
+
+(defn tops [stacks]
+  (str/join (map first stacks)))
 
 (defn -main [& args]
   (let [raw (slurp "resources/input.txt")
         [stacks insts] (parse-input raw)
-        result (perform-insts stacks insts)
-        part1 (str/join (map first result))]
-    (printf "Part 1: %s%n" part1)))
+        part1 (tops (perform-insts reverse stacks insts))
+        part2 (tops (perform-insts identity stacks insts))]
+    (printf "Part 1: %s%n" part1)
+    (printf "Part 2: %s%n" part2)))
