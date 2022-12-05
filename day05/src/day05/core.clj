824ec4a95c1b8b6e4eeb90d2531899945a172ce1
fwcd <fwcdmail@gmail.com>
2022-12-05 14:59:39 +0100

    Parse day 5 input

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -1,7 +1,28 @@
 (ns day05.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(require '[clojure.string :as str])
+
+(defn parse-stacks [lines]
+  (let [ls (drop-last lines)
+        n (/ (+ (count (first ls)) 1) 4)]
+    (map
+      (fn [i] (mapcat
+        (fn [l] (let [c (nth l (+ (* i 4) 1))]
+          (if (= c \ ) [] [c])))
+        ls))
+      (range n))))
+
+(defn parse-insts [lines]
+  (map (fn [l] (rest (re-matches #"move (\d+) from (\d+) to (\d+)" l))) lines))
+
+(defn parse-input [raw]
+  (let [[raw-stacks raw-insts] (map (fn [s] (str/split s #"\n")) (str/split raw #"\n\n"))
+        stacks (parse-stacks raw-stacks)
+        insts (parse-insts raw-insts)]
+    [stacks insts]))
+
+(defn -main [& args]
+  (let [raw (slurp "resources/demo.txt")
+        input (parse-input raw)]
+    (println input)))
