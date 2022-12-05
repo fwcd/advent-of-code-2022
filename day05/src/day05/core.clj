de6d7ed6b8d1da5875c42f6a7c7b40128c256886
fwcd <fwcdmail@gmail.com>
2022-12-05 15:25:11 +0100

    Interpret day 5 instructions

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -6,15 +6,21 @@
 (defn parse-stacks [lines]
   (let [ls (drop-last lines)
         n (/ (+ (count (first ls)) 1) 4)]
-    (map
+    (mapv
       (fn [i] (mapcat
         (fn [l] (let [c (nth l (+ (* i 4) 1))]
           (if (= c \ ) [] [c])))
         ls))
       (range n))))
 
+(defn parse-int [x]
+  (Integer/parseInt x))
+
+(defn parse-inst [line]
+  (map parse-int (rest (re-matches #"move (\d+) from (\d+) to (\d+)" line))))
+
 (defn parse-insts [lines]
-  (map (fn [l] (rest (re-matches #"move (\d+) from (\d+) to (\d+)" l))) lines))
+  (map parse-inst lines))
 
 (defn parse-input [raw]
   (let [[raw-stacks raw-insts] (map (fn [s] (str/split s #"\n")) (str/split raw #"\n\n"))
@@ -22,7 +28,18 @@
         insts (parse-insts raw-insts)]
     [stacks insts]))
 
+(defn perform-inst [stacks [n from to]]
+  (let [i (- from 1)
+        j (- to 1)]
+    (reduce (fn [x f] (f x)) stacks
+      [(fn [s] (update s j (fn [stack] (concat (reverse (take n (nth stacks i))) stack))))
+       (fn [s] (update s i (fn [stack] (drop n stack))))])))
+
+(defn perform-insts [stacks insts]
+  (reduce perform-inst stacks insts))
+
 (defn -main [& args]
   (let [raw (slurp "resources/demo.txt")
-        input (parse-input raw)]
-    (println input)))
+        [stacks insts] (parse-input raw)
+        result (perform-insts stacks insts)]
+    (println result)))
