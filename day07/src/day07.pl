e7602182e87b7f01c3872929ebe6e8576ede19a5
fwcd <fwcdmail@gmail.com>
2022-12-07 15:59:12 +0100

    Construct file tree

--- day07/src/day07.pl
+++ day07/src/day07.pl
@@ -5,7 +5,43 @@ use feature qw(switch);
 
 open(FH, '<', 'resources/demo.txt') or die $!;
 
-my @pwd = ();
+my @pwd;
+my %fs;
+
+sub printTree {
+  my ($indent, $tree) = @_;
+  while (my ($key, $value) = each %$tree) {
+    print "$indent$key -> $value\n";
+    if ("$value" =~ /^HASH.*/) {
+      printTree("$indent  ", $value);
+    }
+  }
+}
+
+sub get {
+  my ($path, $tree) = @_;
+  my $len = scalar(@$path);
+  if ($len > 0) {
+    my @subpath = (@$path)[1..($len - 1)];
+    my $subtree = $tree->{$path->[0]};
+    return get(\@subpath, $subtree);
+  } else {
+    return $tree;
+  }
+}
+
+sub insertFile {
+  my ($name, $size) = @_;
+  my ($tree) = get(\@pwd, \%fs);
+  $tree->{$name} = $size;
+}
+
+sub insertDir {
+  my ($name) = @_;
+  print "Pwd: @pwd\n";
+  my ($tree) = get(\@pwd, \%fs);
+  $tree->{$name} = {};
+}
 
 sub cd {
   my ($dest) = @_;
@@ -26,9 +62,14 @@ while (<FH>) {
     }
   } elsif (my ($size, $name) = ($_ =~ m/(\d+)\s+(.+)/)) {
     print "File $name of size $size\n";
+    insertFile($name, $size);
   } elsif (my ($name) = ($_ =~ m/dir\s+(.+)/)) {
     print "Directory $name\n";
+    insertDir($name);
   }
+  print "<tree>\n";
+  printTree("", \%fs);
+  print "</tree>\n";
 }
 
 close(FH);
