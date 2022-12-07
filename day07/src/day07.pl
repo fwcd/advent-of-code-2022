03667421d36937fe59cc8bafe28e6624ec8ba09b
fwcd <fwcdmail@gmail.com>
2022-12-07 14:52:59 +0100

    Implement cding

--- day07/src/day07.pl
+++ day07/src/day07.pl
@@ -1,16 +1,33 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use feature qw(switch);
 
 open(FH, '<', 'resources/demo.txt') or die $!;
 
+my @pwd = ();
+
+sub cd {
+  my ($dest) = @_;
+  given ($dest) {
+    when ('.') {}
+    when ('..') { pop @pwd; }
+    when ('/') { @pwd = (); }
+    default { push @pwd, $dest; }
+  }
+}
+
 while (<FH>) {
-  if (my ($command, $args) = ($_ =~ m/\$\s+(\w+)\s*(.*)/)) {
-    print "$command with args $args\n";
+  if (my ($command, $arg) = ($_ =~ m/\$\s+(\w+)\s*(.*)/)) {
+    if ($command eq 'cd') {
+      cd($arg);
+      my $path = join '/', @pwd;
+      print "Now at /$path\n";
+    }
   } elsif (my ($size, $name) = ($_ =~ m/(\d+)\s+(.+)/)) {
-    print "File $name of size $size\n"
+    print "File $name of size $size\n";
   } elsif (my ($name) = ($_ =~ m/dir\s+(.+)/)) {
-    print "Directory $name\n"
+    print "Directory $name\n";
   }
 }
 
