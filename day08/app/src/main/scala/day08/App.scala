cd133bedb7520bdd6bdc345f85dcaca34896cfd1
fwcd <fwcdmail@gmail.com>
2022-12-08 06:42:06 +0100

    Write 8.1 in scala

--- day08/app/src/main/scala/day08/App.scala
+++ day08/app/src/main/scala/day08/App.scala
@@ -3,8 +3,36 @@ package day08
 import scala.io.Source
 
 object App {
+  type Pos = (Int, Int)
+
+  def filterVisible(seq: Iterable[(Pos, Int)]): Iterable[Pos] = seq
+    .scanLeft(((-1, -1), -1)) { (acc, elem) => if (elem._2 > acc._2) { elem } else { acc } }
+    .filter { acc => acc._2 >= 0 }
+    .toList
+    .distinct
+    .map { acc => acc._1 }
+
+  def rows(grid: List[List[Int]]): Iterable[Iterable[(Pos, Int)]] = grid
+    .zipWithIndex
+    .map { row => row._1.zipWithIndex.map { cell => ((row._2, cell._2), cell._1) } }
+
+  def cols(grid: List[List[Int]]): Iterable[Iterable[(Pos, Int)]] = rows(grid.transpose)
+    .map { col => col.map { v => (v._1.swap, v._2) } }
+
+  def countTotalVisible(grid: List[List[Int]]) =
+    (rows(grid).concat(cols(grid)))
+      .flatMap { v => List(v, v.toList.reverse) }
+      .flatMap { v => filterVisible(v) }
+      .toList
+      .distinct
+      .length
+
   def main(args: Array[String]): Unit = {
-    val lines = Source.fromResource("demo.txt").getLines().toList
-    println(lines)
+    val grid = Source.fromResource("input.txt").getLines()
+      .map { s => s.map { c => c.toInt }.toList }
+      .toList
+    val part1 = countTotalVisible(grid)
+
+    println(s"Part 1: $part1")
   }
 }
