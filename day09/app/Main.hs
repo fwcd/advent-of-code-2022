1b8f2df89a50220bda126d9787de94029cdc7c21
fwcd <fwcdmail@gmail.com>
2022-12-09 06:16:13 +0100

    Parse insts

--- day09/app/Main.hs
+++ day09/app/Main.hs
@@ -1,6 +1,40 @@
 module Main (main) where
 
+import qualified Data.Set as S
+import Data.Maybe (mapMaybe)
+
+data Pos = Pos Int Int
+
+combine :: (Int -> Int -> Int) -> Pos -> Pos -> Pos
+combine op (Pos x1 y1) (Pos x2 y2) = Pos (op x1 y1) (op x2 y2)
+
+(+.) :: Pos -> Pos -> Pos
+(+.) = combine (+)
+
+(-.) :: Pos -> Pos -> Pos
+(-.) = combine (-)
+
+data BridgeState = BridgeState
+  { tailPos :: Pos
+  , headPos :: Pos
+  , visited :: S.Set Pos
+  }
+
+data Dir = R | U | L | D
+  deriving (Read, Show, Eq, Ord)
+
+data Inst = Inst Dir Int
+  deriving (Read, Show, Eq, Ord)
+
+parseInst :: String -> Maybe Inst
+parseInst (d:' ':c) = Just $ Inst (read [d]) (read c)
+parseInst _ = Nothing
+
+perform :: Inst -> BridgeState -> BridgeState
+perform (Inst dir n) = undefined
+
 main :: IO ()
 main = do
   lines <- lines <$> readFile "resources/demo.txt"
-  mapM_ putStrLn lines
+  let insts = mapMaybe parseInst lines
+  mapM_ print insts
