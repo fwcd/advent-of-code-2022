a84c1ba03909174f2b9e24293b5787a0db163393
fwcd <fwcdmail@gmail.com>
2022-12-09 07:04:13 +0100

    Begin implementing 9.1

--- day09/app/Main.hs
+++ day09/app/Main.hs
@@ -4,23 +4,37 @@ import qualified Data.Set as S
 import Data.Maybe (mapMaybe)
 
 data Pos = Pos Int Int
+  deriving (Show, Eq, Ord)
 
-combine :: (Int -> Int -> Int) -> Pos -> Pos -> Pos
-combine op (Pos x1 y1) (Pos x2 y2) = Pos (op x1 y1) (op x2 y2)
+norm :: Pos -> Int
+norm (Pos i j) = abs (i + j)
 
-(+.) :: Pos -> Pos -> Pos
-(+.) = combine (+)
+dist :: Pos -> Pos -> Int
+dist (Pos i1 j1) (Pos i2 j2) = norm $ Pos (i2 - i1) (j2 - j1)
 
-(-.) :: Pos -> Pos -> Pos
-(-.) = combine (-)
+range :: Pos -> Pos -> [Pos]
+range (Pos i1 j1) (Pos i2 j2) | i1 == i2  = Pos i1 <$> [min j1 j2..max j1 j2]
+                              | j1 == j2  = flip Pos j1 <$> [min i1 i2..max i1 i2]
+                              | otherwise = error "Can only perform range on axis-aligned positions"
+
+insertAll :: Ord a => [a] -> S.Set a -> S.Set a
+insertAll xs s = foldr S.insert s xs
 
 data BridgeState = BridgeState
   { tailPos :: Pos
   , headPos :: Pos
   , visited :: S.Set Pos
   }
+  deriving (Show, Eq, Ord)
 
-data Dir = R | U | L | D
+initialState :: BridgeState
+initialState = BridgeState
+  { headPos = Pos 0 0
+  , tailPos = Pos 0 0
+  , visited = S.fromList [Pos 0 0]
+  }
+
+data Dir = L | R | U | D
   deriving (Read, Show, Eq, Ord)
 
 data Inst = Inst Dir Int
@@ -30,11 +44,48 @@ parseInst :: String -> Maybe Inst
 parseInst (d:' ':c) = Just $ Inst (read [d]) (read c)
 parseInst _ = Nothing
 
-perform :: Inst -> BridgeState -> BridgeState
-perform (Inst dir n) = undefined
+moveHorizontally :: Int -> BridgeState -> BridgeState
+moveHorizontally n s = BridgeState { headPos = h', tailPos = t', visited = v' }
+  where
+    h@(Pos h1 h2) = headPos s
+    t@(Pos t1 t2) = tailPos s
+    v   = visited s
+    v'  = insertAll dv v
+    h2' = h2 + n
+    t2' = h2' + signum (t2 - h2')
+    h' = Pos h1 h2'
+    (t', dv) | dist h' t <= 1 = (t, [])
+             | otherwise      = (Pos h1 t2', v')
+                where v' | h1 == t1  = range t t'
+                         | otherwise = range (Pos h1 (t2 + signum n)) t'
+
+-- TODO: Implement moveVertically in terms of moveHorizontally with parameter swapping
+
+moveVertically :: Int -> BridgeState -> BridgeState
+moveVertically n s = BridgeState { headPos = h', tailPos = t', visited = v' }
+  where
+    h@(Pos h1 h2) = headPos s
+    t@(Pos t1 t2) = tailPos s
+    v   = visited s
+    v'  = insertAll dv v
+    h1' = h1 + n
+    t1' = h1' + signum (t1 - h1')
+    h' = Pos h1' h2
+    (t', dv) | dist h' t <= 1 = (t, [])
+             | otherwise      = (Pos t1' h2, v')
+                where v' | h2 == t2  = range t t'
+                         | otherwise = range (Pos (t1 + signum n) h2) t'
+
+performInst :: Inst -> BridgeState -> BridgeState
+performInst (Inst d n) = case d of
+  L -> moveHorizontally (-n)
+  R -> moveHorizontally n
+  U -> moveVertically (-n)
+  D -> moveVertically n
 
 main :: IO ()
 main = do
   lines <- lines <$> readFile "resources/demo.txt"
   let insts = mapMaybe parseInst lines
-  mapM_ print insts
+      finalState = foldl (flip performInst) initialState insts
+  print (S.size (visited finalState))
