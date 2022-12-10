ec7472371f82490a9afe01839316e1f5d327c3ed
fwcd <fwcdmail@gmail.com>
2022-12-10 21:59:18 +0100

    Parse instructions

--- day10/src/day10.c
+++ day10/src/day10.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BUFFER_SIZE 255
+
+enum Op {
+  NOOP, ADDX
+};
+
+struct Inst {
+  enum Op op;
+  int value;
+};
+
+struct Inst parse_inst(const char *raw) {
+  switch (raw[0]) {
+  case 'n': return (struct Inst) { .op = NOOP, .value = 0 };
+  case 'a': return (struct Inst) { .op = ADDX, .value = atoi(raw + 5) };
+  }
+  fprintf(stderr, "Could not parse instruction '%s'\n", raw);
+  exit(1);
+}
 
 int main(void) {
   FILE *fp = fopen("resources/demo.txt", "r");
 
   char buffer[BUFFER_SIZE];
   while (fgets(buffer, BUFFER_SIZE, fp)) {
-    printf("%s", buffer);
+    struct Inst inst = parse_inst(buffer);
+    printf("%d %d\n", inst.op, inst.value);
   }
 
   fclose(fp);
