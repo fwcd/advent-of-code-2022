3a57655451ebbe09a3dc8bc6bad22b64f6098b13
fwcd <fwcdmail@gmail.com>
2022-12-12 01:39:12 +0100

    Parse day 11 op

--- day11/lib/day11.ex
+++ day11/lib/day11.ex
@@ -1,22 +1,39 @@
 defmodule Day11 do
+  def parse_op("new = " <> raw_expr) do
+    [raw_lhs, raw_op, raw_rhs] = raw_expr |> String.split(" ")
+    fn old, new ->
+      resolve = fn raw ->
+        case raw do
+          "old" -> old
+          "new" -> new
+          lit   -> String.to_integer(lit)
+        end
+      end
+      op = case raw_op do
+        "*" -> &*/2
+        "+" -> &+/2
+      end
+      op.(resolve.(raw_lhs), resolve.(raw_rhs))
+    end
+  end
+
   def parse_monkey(raw) do
     raw
       |> String.split("\n")
       |> Enum.reduce(%{}, fn line, acc ->
         case String.trim(line) do
-          "Starting items: " <> items       -> acc |> Map.put(:items, items |> String.split(", ") |> Enum.map(&String.to_integer/1))
-          "Operation: " <> op               -> acc |> Map.put(:op, op)
-          "Test: divisible by " <> n        -> acc |> Map.put(:test_divisor, String.to_integer(n))
-          "If true: throw to monkey " <> n  -> acc |> Map.put(:if_true, String.to_integer(n))
-          "If false: throw to monkey " <> n -> acc |> Map.put(:if_false, String.to_integer(n))
-          _                                 -> acc
+          "Starting items: " <> raw_items       -> acc |> Map.put(:items, raw_items |> String.split(", ") |> Enum.map(&String.to_integer/1))
+          "Operation: " <> raw_rule             -> acc |> Map.put(:op, raw_rule |> parse_op)
+          "Test: divisible by " <> raw_n        -> acc |> Map.put(:test_divisor, raw_n |> String.to_integer)
+          "If true: throw to monkey " <> raw_n  -> acc |> Map.put(:if_true, raw_n |> String.to_integer)
+          "If false: throw to monkey " <> raw_n -> acc |> Map.put(:if_false, raw_n |> String.to_integer)
+          _                                     -> acc
         end
       end)
   end
 
   def step(monkey, monkeys) do
     monkeys
-    # TODO
   end
 
   def simulate_round(monkeys) do
@@ -42,7 +59,7 @@ defmodule Day11 do
       |> simulate(20)
       |> Enum.sort(&(&1 >= &2))
 
-    part1 = Enum.at(inspects, 0) * Enum.at(inspects, 1)
+    part1 = (inspects |> Enum.at 0) * (inspects |> Enum.at 1)
     IO.puts "Part 1: #{part1}"
   end
 end
