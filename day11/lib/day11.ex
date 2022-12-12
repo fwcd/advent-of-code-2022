86f45e150efd662bc8773b9964b94d63e965f0cc
fwcd <fwcdmail@gmail.com>
2022-12-12 01:29:21 +0100

    Set up some infrastructure for day 11 simulation

--- day11/lib/day11.ex
+++ day11/lib/day11.ex
@@ -14,11 +14,35 @@ defmodule Day11 do
       end)
   end
 
+  def step(monkey, monkeys) do
+    monkeys
+    # TODO
+  end
+
+  def simulate_round(monkeys) do
+    monkeys |> List.foldl(monkeys, &step/2)
+  end
+
+  def simulate(monkeys, rounds, inspects \\ nil) do
+    if rounds > 0 do
+      inspects = Enum.zip(monkeys, inspects || List.duplicate(0, length(monkeys)))
+        |> Enum.map(fn {monkey, n} -> n + length(monkey[:items]) end)
+      simulate_round(monkeys) |> simulate(rounds - 1, inspects)
+    else
+      inspects
+    end
+  end
+
   def main do
     monkeys = File.read!("resources/demo.txt")
       |> String.split("\n\n")
       |> Enum.map(&parse_monkey/1)
-    monkeys
-      |> Enum.map(&IO.inspect/1)
+    
+    inspects = monkeys
+      |> simulate(20)
+      |> Enum.sort(&(&1 >= &2))
+
+    part1 = Enum.at(inspects, 0) * Enum.at(inspects, 1)
+    IO.puts "Part 1: #{part1}"
   end
 end
