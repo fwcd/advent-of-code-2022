0ab625f18750ebc297c534885f10550d43327e6d
fwcd <fwcdmail@gmail.com>
2022-12-12 06:06:02 +0100

    Read day 12 demo input

--- day12/app/src/main/java/dev/fwcd/aoc2022/day12/App.java
+++ day12/app/src/main/java/dev/fwcd/aoc2022/day12/App.java
@@ -1,7 +1,16 @@
 package dev.fwcd.aoc2022.day12;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(App.class.getResourceAsStream("/demo.txt")))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
     }
 }
