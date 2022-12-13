7dab83f6e68a95b64038330760b26dfa360f1e11
fwcd <fwcdmail@gmail.com>
2022-12-13 06:23:21 +0100

    Solve 13.1

---
+++ day13/src/day13.js
@@ -0,0 +1,39 @@
+const fs = require('fs').promises;
+
+function compare(a, b) {
+  if (typeof a == 'number' && typeof b == 'number') {
+    return a < b ? -1 : a > b ? 1 : 0;
+  } else if (typeof a == 'number') {
+    return compare([a], b);
+  } else if (typeof b == 'number') {
+    return compare(a, [b]);
+  } else {
+    for (let i = 0; i < Math.max(a.length, b.length); i++) {
+      if (i >= a.length) {
+        return -1;
+      } else if (i >= b.length) {
+        return 1;
+      }
+      const cmp = compare(a[i], b[i]);
+      if (cmp !== 0) {
+        return cmp;
+      }
+    }
+    return 0;
+  }
+}
+
+(async () => {
+  const input = await fs.readFile('resources/input.txt', { encoding: 'utf8' });
+  const pairs = input.split('\n\n')
+    .map(rawPair => rawPair
+      .split('\n')
+      .filter(line => line.trim())
+      .flatMap(line => line ? [JSON.parse(line)] : []));
+  
+  const part1 = pairs
+    .flatMap(([l, r], i) => compare(l, r) <= 0 ? [i + 1] : [])
+    .reduce((x, y) => x + y);
+  
+  console.log(`Part 1: ${part1}`);
+})();
