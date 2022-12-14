fb1f6f5dc9808f6c0c538e83306e1f052a7d7c0e
fwcd <fwcdmail@gmail.com>
2022-12-14 18:46:43 +0100

    Implement the sand simulation

--- day14/bin/main.ml
+++ day14/bin/main.ml
@@ -3,19 +3,22 @@
 [@@@warnerror "-unused-var-strict"]
 
 type pos = { x: int; y: int }
-type field = Rock | Space
+type field = Rock | Sand | Space
 type cave = { map: field array; width: int; height: int; top_left: pos }
+type state = { cave: cave; spawn_pos: pos; landed_sand: int; reached_void: bool }
 
 let (<<) f g x = f (g x);;
-
-let flip f x y = f y x;;
-
+let flip f y x = f x y;;
 let range n = List.map (fun x -> x - 1) (List.init n (fun x -> x + 1));;
 
+let rec any f xs = match xs with
+  | []      -> false
+  | x :: xs -> if f x then true else any f xs
+
 let rec drop_last xs = match xs with
-  | []        -> []
-  | [_]       -> []
-  | (x :: xs) -> x :: drop_last xs
+  | []       -> []
+  | [_]      -> []
+  | x :: xs -> x :: drop_last xs
   ;;
 
 let pos_of_list (l: int list): pos = match l with
@@ -33,7 +36,8 @@ let zip_pos (f: int -> int -> int) (p1: pos) (p2: pos): pos = { x = f p1.x p2.x;
 let row (y: int) (cave: cave): field array = Array.sub cave.map (y * cave.width) cave.width;;
 
 let char_of_field (field: field): char = match field with
-  | Rock -> '#'
+  | Rock  -> '#'
+  | Sand  -> 'o'
   | Space -> ' '
   ;;
 
@@ -52,6 +56,14 @@ let index (pos: pos) (cave: cave): int =
   if in_bounds pos cave
     then ((pos.y - cave.top_left.y) * cave.width) + (pos.x - cave.top_left.x)
     else failwith (String.concat " " [pretty_pos pos ;"out of bounds, expected between"; pretty_pos cave.top_left; "and"; pretty_pos (zip_pos (+) cave.top_left { x = cave.width; y = cave.height })])
+  ;;
+
+let get (pos: pos) (cave: cave): field = Array.get cave.map (index pos cave);;
+
+let place (field: field) (pos: pos) (cave: cave): cave =
+  let new_map = Array.copy cave.map in
+  Array.set new_map (index pos cave) field;
+  { cave with map = new_map }
   ;;
 
 let draw_line ((start_pos: pos), (end_pos: pos)) (cave: cave) =
@@ -73,14 +85,36 @@ let draw_path (path: pos list) (cave: cave) =
   List.fold_right draw_line (List.combine (drop_last path) (List.tl path)) cave
   ;;
 
-let parse_cave (lines: string list): cave =
+let parse_cave (lines: string list) (spawn_pos: pos): cave =
   let paths: pos list list = List.map parse_line lines in
-  let poss: pos list = List.flatten paths in
+  let poss: pos list = spawn_pos :: List.flatten paths in
   let top_left = List.fold_right (zip_pos min) poss { x = max_int; y = max_int } in
   let bottom_right = List.fold_right (zip_pos max) poss { x = min_int; y = min_int } in
   let size = zip_pos (+) (zip_pos (-) bottom_right top_left) { x = 1; y = 1 } in
   let initial = { map = Array.make (size.x * size.y) Space; width = size.x; height = size.y; top_left = top_left } in
   List.fold_right draw_path paths initial
+  ;;
+
+let is_free (pos: pos) (cave: cave): bool = get pos cave == Space;;
+
+let fall (dx: int) (pos: pos): pos = { x = pos.x + dx; y = pos.y + 1 };;
+
+let rec pour_sand (pos: pos) (state: state): state =
+  let below = List.map (flip fall pos) [0; -1; 1] in
+  if any (not << flip in_bounds state.cave) below then
+    { state with reached_void = true }
+  else
+    let free = List.filter (flip is_free state.cave) below in
+    match free with
+      | []     -> { state with cave = place Sand pos state.cave; landed_sand = state.landed_sand + 1 }
+      | f :: _ -> pour_sand f state
+  ;;
+
+let rec simulate (state: state): state =
+  if state.reached_void then
+    state
+  else
+    simulate (pour_sand state.spawn_pos state)
   ;;
 
 let read_lines (filename: string): string list =
@@ -98,6 +132,9 @@ let read_lines (filename: string): string list =
 
 let () =
   let lines = read_lines "resources/demo.txt" in
-  let cave = parse_cave lines in
-  Printf.printf "Cave: \n%s\n" (pretty_cave cave)
+  let spawn_pos = { x = 500; y = 0 } in
+  let initial = { cave = parse_cave lines spawn_pos; spawn_pos = spawn_pos; landed_sand = 0; reached_void = false } in
+  let final = simulate initial in
+  Printf.printf "Cave: \n%s\n" (pretty_cave final.cave);
+  Printf.printf "Part 1: %d\n" final.landed_sand
   ;;
