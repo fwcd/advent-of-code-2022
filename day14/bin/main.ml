5bebbaebfcc35ed5f2ce19fec4db343320679084
fwcd <fwcdmail@gmail.com>
2022-12-14 17:30:51 +0100

    Fix indent

--- day14/bin/main.ml
+++ day14/bin/main.ml
@@ -88,11 +88,11 @@ let read_lines (filename: string): string list =
   let lines = ref [] in
   let channel = open_in filename in
   try
-      while true; do
-          lines := input_line channel :: !lines
-      done; !lines
+    while true; do
+      lines := input_line channel :: !lines
+    done; !lines
   with End_of_file ->
-      close_in channel;
+    close_in channel;
   List.rev !lines
   ;;
 
