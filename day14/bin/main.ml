3e0e8fb10535ce3df5c44256ba1e6318ffd811cf
fwcd <fwcdmail@gmail.com>
2022-12-14 14:27:34 +0100

    Read day 14 input

--- day14/bin/main.ml
+++ day14/bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+let read_lines filename =
+  (* https://stackoverflow.com/questions/5774934/how-do-i-read-in-lines-from-a-text-file-in-ocaml/5775024#5775024 *)
+  let lines = ref [] in
+  let channel = open_in filename in
+  try
+      while true; do
+          lines := input_line channel :: !lines
+      done; !lines
+  with End_of_file ->
+      close_in channel;
+  List.rev !lines;;
+
+let () =
+  let lines = read_lines "resources/demo.txt" in
+  Printf.printf "Lines: %s\n" (String.concat "\n" lines)
