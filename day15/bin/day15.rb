f9095e518e67480f474c9fb609a449d57314e305
fwcd <fwcdmail@gmail.com>
2022-12-15 18:22:32 +0100

    Solve day 15 part 1

--- day15/bin/day15.rb
+++ day15/bin/day15.rb
@@ -2,11 +2,29 @@
 
 require 'set'
 
-sensors = File.readlines('resources/demo.txt')
+def manhattan(p, q)
+  (p[0] - q[0]).abs + (p[1] - q[1]).abs
+end
+
+def solve(y, sensors)
+  beacons = sensors
+    .map { |s, b| b }
+    .to_set
+  sensors
+    .map { |s, b| [s, b, manhattan(s, b) - (s[1] - y).abs] }
+    .filter { |s, b, d| d > 0 }
+    .flat_map { |s, b, d| ((s[0] - d)..(s[0] + d)).map { |x| [x, y] } }
+    .to_set
+    .filter { |p| !beacons.include?(p) }
+    .length
+end
+
+sensors = File.readlines('resources/input.txt')
   .map { |line|
-    line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
+    sx, sy, bx, by = line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
       .captures
       .map { |s| s.to_i }
+    [[sx, sy], [bx, by]]
   }
 
-puts sensors.map { |s| s.join(', ') }
+puts "Part 1: #{solve(2000000, sensors)}"
