8c0a0ebc668418ec4a30d87f5952673af05e2dfb
fwcd <fwcdmail@gmail.com>
2022-12-15 19:03:36 +0100

    Try solving 15.1 using interval arithmetic

--- day15/bin/day15.rb
+++ day15/bin/day15.rb
@@ -6,17 +6,34 @@ def manhattan(p, q)
   (p[0] - q[0]).abs + (p[1] - q[1]).abs
 end
 
-def solve(y, sensors)
-  beacons = sensors
-    .map { |s, b| b }
-    .to_set
+class Range
+  def overlaps?(other)
+    other.include?(self.min) || other.include?(self.max)
+  end
+
+  def merge(other)
+    [self.min, other.min].min..[self.max, other.max].max
+  end
+end
+
+def scan(y, sensors)
   sensors
     .map { |s, b| [s, b, manhattan(s, b) - (s[1] - y).abs] }
     .filter { |s, b, d| d > 0 }
-    .flat_map { |s, b, d| ((s[0] - d)..(s[0] + d)).to_a }
+    .sort_by { |s, b, d| s[0] }
+    .map { |s, b, d| ((s[0] - d)..(s[0] + d)) }
+    .reduce([]) { |rs, r2| if rs.last&.overlaps?(r2) then [*rs[0...-1], rs.last.merge(r2)] else [*rs, r2] end }
+end
+
+def invalid_position_count(y, sensors)
+  beacons = sensors
+    .map { |s, b| b }
+    .filter { |b| b[1] == y }
     .to_set
-    .filter { |x| !beacons.include?([x, y]) }
     .length
+  scan(y, sensors)
+    .map { |r| r.size }
+    .sum - beacons
 end
 
 sensors = File.readlines('resources/input.txt')
@@ -27,4 +44,4 @@ sensors = File.readlines('resources/input.txt')
     [[sx, sy], [bx, by]]
   }
 
-puts "Part 1: #{solve(2000000, sensors)}"
+puts "Part 1: #{invalid_position_count(2_000_000, sensors)}"
