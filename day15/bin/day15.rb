1893390d6bee0fa565a7e501f53a6cf9669dc726
fwcd <fwcdmail@gmail.com>
2022-12-15 17:57:39 +0100

    Parse day 15 input

--- day15/bin/day15.rb
+++ day15/bin/day15.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
 
-puts "Hello world!"
+require 'set'
+
+sensors = File.readlines('resources/demo.txt')
+  .map { |line|
+    line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
+      .captures
+      .map { |s| s.to_i }
+  }
+
+puts sensors.map { |s| s.join(', ') }
