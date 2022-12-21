0eeb4d3322febe7876ffb5cd84926fa7d6e71a10
fwcd <fwcdmail@gmail.com>
2022-12-21 19:22:53 +0100

    Add first attempt at day 20

--- day20/src/day20.m
+++ day20/src/day20.m
@@ -1,9 +1,9 @@
 #import <Foundation/Foundation.h>
 
-NSMutableArray *readInput() {
+NSMutableArray<NSNumber *> *readInput() {
   NSString *raw = [NSString stringWithContentsOfFile:@"resources/demo.txt" encoding:NSUTF8StringEncoding error:nil];
-  NSArray *lines = [raw componentsSeparatedByString:@"\n"];
-  NSMutableArray *input = [[NSMutableArray alloc] init];
+  NSArray<NSString *> *lines = [raw componentsSeparatedByString:@"\n"];
+  NSMutableArray<NSNumber *> *input = [[NSMutableArray alloc] init];
   for (NSString *line in lines) {
     NSString *trimmed = [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     if ([trimmed length] > 0) {
@@ -13,10 +13,75 @@ NSMutableArray *readInput() {
   return input;
 }
 
+NSMutableArray<NSNumber *> *range(int n) {
+  NSMutableArray<NSNumber *> *result = [[NSMutableArray alloc] init];
+  for (int i = 0; i < n; i++) {
+    [result addObject:[NSNumber numberWithInt:i]];
+  }
+  return result;
+}
+
+void swap(NSMutableArray *array, int i, int j) {
+  id tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+
+NSMutableArray *permuted(NSMutableArray *array, NSArray<NSNumber *> * permutation) {
+  NSMutableArray<NSNumber *> *result = [[NSMutableArray alloc] init];
+  for (int i = 0; i < [array count]; i++) {
+    [result addObject:array[[permutation[i] intValue]]];
+  }
+  return result;
+}
+
+NSMutableArray<NSNumber *> *inverse(NSArray<NSNumber *> *permutation) {
+  NSMutableArray<NSNumber *> *result = [NSMutableArray arrayWithArray:permutation];
+  for (int i = 0; i < [permutation count]; i++) {
+    result[[permutation[i] intValue]] = [NSNumber numberWithInt:i];
+  }
+  return result;
+}
+
+int mod(int n, int m) {
+  return (n % m + m) % m;
+}
+
+NSMutableArray<NSNumber *> *mix(NSMutableArray<NSNumber *> *ciphertext) {
+  NSMutableArray<NSNumber *> *permutation = range([ciphertext count]);
+  int n = [ciphertext count];
+
+  for (int i = 0; i < n; i++) {
+    int newIndex = [permutation[i] intValue];
+    int delta = [ciphertext[i] intValue];
+    int step = delta >= 0 ? 1 : -1;
+
+    // Capture permutation[newIndex], permutation[newIndex + 1], ..., permutation[newIndex + delta]
+    // (with indices taken mod n) before rewriting the permutation in-place.
+    NSMutableArray<NSNumber *> *previousPermutationWindow = [[NSMutableArray alloc] init];
+    for (int i = 0; i <= abs(delta); i++) {
+      [previousPermutationWindow addObject:permutation[mod(newIndex + i * step, n)]];
+    }
+
+    // Now apply the move (with mapped indices).
+    for (int i = abs(delta); i > 0; i--) {
+      permutation[[previousPermutationWindow[i] intValue]] = [NSNumber numberWithInt:mod(newIndex + (i - 1) * step, n)];
+    }
+    permutation[[previousPermutationWindow[0] intValue]] = [NSNumber numberWithInt:mod(newIndex + delta, n)];
+
+    NSLog(@"%d moves from new index %d:\t%@ (window: %@)", delta, newIndex, [permuted(ciphertext, permutation) componentsJoinedByString:@", "], [previousPermutationWindow componentsJoinedByString:@" "]);
+  }
+
+  // FIXME: Negative deltas behave strangely
+
+  return permuted(ciphertext, permutation);
+}
+
 int main(void) {
   @autoreleasepool {
-    NSMutableArray *input = readInput();
-    NSLog(@"%@", input);
+    NSMutableArray<NSNumber *> *ciphertext = readInput();
+    NSArray <NSNumber *>* plaintext = mix(ciphertext);
+    NSLog(@"%@", [plaintext componentsJoinedByString:@" "]);
     return 0;
   }
 }
