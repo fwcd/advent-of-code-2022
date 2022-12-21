7af785eba7b45a45ae4e335db1be77029118faf1
fwcd <fwcdmail@gmail.com>
2022-12-21 15:53:02 +0100

    Parse day 20 input

--- day20/src/day20.m
+++ day20/src/day20.m
@@ -1,6 +1,22 @@
 #import <Foundation/Foundation.h>
 
+NSMutableArray *readInput() {
+  NSString *raw = [NSString stringWithContentsOfFile:@"resources/demo.txt" encoding:NSUTF8StringEncoding error:nil];
+  NSArray *lines = [raw componentsSeparatedByString:@"\n"];
+  NSMutableArray *input = [[NSMutableArray alloc] init];
+  for (NSString *line in lines) {
+    NSString *trimmed = [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if ([trimmed length] > 0) {
+      [input addObject:[NSNumber numberWithInt:[trimmed intValue]]];
+    }
+  }
+  return input;
+}
+
 int main(void) {
-  NSLog(@"Hello world!");
-  return 0;
+  @autoreleasepool {
+    NSMutableArray *input = readInput();
+    NSLog(@"%@", input);
+    return 0;
+  }
 }
