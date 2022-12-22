257602717fd85935d7c94988ccee295eb14fa4e5
fwcd <fwcdmail@gmail.com>
2022-12-22 04:47:51 +0100

    Use longs (almost) everywhere in day 20

--- day20/src/day20.m
+++ day20/src/day20.m
@@ -7,21 +7,21 @@ NSMutableArray<NSNumber *> *readInput(NSString *filePath) {
   for (NSString *line in lines) {
     NSString *trimmed = [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     if ([trimmed length] > 0) {
-      [input addObject:[NSNumber numberWithInt:[trimmed intValue]]];
+      [input addObject:[NSNumber numberWithLong:[trimmed intValue]]];
     }
   }
   return input;
 }
 
-NSMutableArray<NSNumber *> *range(int n) {
+NSMutableArray<NSNumber *> *range(long n) {
   NSMutableArray<NSNumber *> *result = [[NSMutableArray alloc] init];
-  for (int i = 0; i < n; i++) {
-    [result addObject:[NSNumber numberWithInt:i]];
+  for (long i = 0; i < n; i++) {
+    [result addObject:[NSNumber numberWithLong:i]];
   }
   return result;
 }
 
-void swap(NSMutableArray *array, int i, int j) {
+void swap(NSMutableArray *array, long i, long j) {
   id tmp = array[i];
   array[i] = array[j];
   array[j] = tmp;
@@ -29,16 +29,16 @@ void swap(NSMutableArray *array, int i, int j) {
 
 NSMutableArray *permuted(NSArray *array, NSArray<NSNumber *> * permutation) {
   NSMutableArray<NSNumber *> *result = [NSMutableArray arrayWithArray:array];
-  for (int i = 0; i < [permutation count]; i++) {
-    result[[permutation[i] intValue]] = array[i];
+  for (long i = 0; i < [permutation count]; i++) {
+    result[[permutation[i] longValue]] = array[i];
   }
   return result;
 }
 
 NSMutableArray<NSNumber *> *inverted(NSArray<NSNumber *> *permutation) {
   NSMutableArray<NSNumber *> *result = [NSMutableArray arrayWithArray:permutation];
-  for (int i = 0; i < [permutation count]; i++) {
-    result[[permutation[i] intValue]] = [NSNumber numberWithInt:i];
+  for (long i = 0; i < [permutation count]; i++) {
+    result[[permutation[i] longValue]] = [NSNumber numberWithLong:i];
   }
   return result;
 }
@@ -52,45 +52,46 @@ struct MixState {
   NSMutableArray<NSNumber *> *inversePermutation;
 };
 
-void mix(NSArray<NSNumber *> *moves, int n, struct MixState *state) {
+void mix(NSArray<NSNumber *> *moves, long n, struct MixState *state) {
   // We track permutation and inverse permutation separately. This is to
   // avoid computing the inverse permutation in every iteration (resulting in O(n^2)).
   // This way we only have O(n * max abs(delta)).
 
-  for (int i = 0; i < n; i++) {
-    int startIndex = [state->permutation[i] intValue];
-    int move = [moves[i] intValue];
-    int endIndex = startIndex + move;
+  for (long i = 0; i < n; i++) {
+    long startIndex = [state->permutation[i] longValue];
+    long move = [moves[i] longValue];
+    long endIndex = startIndex + move;
 
     // Deal with weird boundary wrapping
     endIndex = mod(endIndex - 1, n - 1) + 1;
 
-    int delta = endIndex - startIndex;
-    int step = delta >= 0 ? 1 : -1;
+    long delta = endIndex - startIndex;
+    long absDelta = labs(delta);
+    long step = delta >= 0 ? 1 : -1;
 
     // Compose the move onto our permutation.
-    state->permutation[[state->inversePermutation[startIndex] intValue]] = [NSNumber numberWithInt:mod(endIndex, n)];
-    for (int i = 1; i <= abs(delta); i++) {
-      state->permutation[[state->inversePermutation[mod(startIndex + i * step, n)] intValue]] = [NSNumber numberWithInt:mod(startIndex + (i - 1) * step, n)];
+    state->permutation[[state->inversePermutation[startIndex] longValue]] = [NSNumber numberWithLong:mod(endIndex, n)];
+    for (long i = 1; i <= absDelta; i++) {
+      state->permutation[[state->inversePermutation[mod(startIndex + i * step, n)] longValue]] = [NSNumber numberWithLong:mod(startIndex + (i - 1) * step, n)];
     }
 
     // Compose the move onto our inverse permutation (this is the easier one since we can perform
     // the move directly on the array rather than needing the extra level of index mapping).
     NSNumber *tmp = state->inversePermutation[mod(startIndex, n)];
-    for (int i = 0; i < abs(delta); i++) {
+    for (long i = 0; i < absDelta; i++) {
       state->inversePermutation[mod(startIndex + i * step, n)] = state->inversePermutation[mod(startIndex + (i + 1) * step, n)];
     }
     state->inversePermutation[endIndex] = tmp;
   }
 }
 
-int solve(NSArray<NSNumber *> *ciphertext, long factor, int rounds) {
-  int n = [ciphertext count];
-  int zeroIndex = [ciphertext indexOfObject:[NSNumber numberWithLong:0]];
+long solve(NSArray<NSNumber *> *ciphertext, long factor, long rounds) {
+  long n = [ciphertext count];
+  long zeroIndex = [ciphertext indexOfObject:[NSNumber numberWithLong:0]];
 
   // Scale ciphertext by the given factor
   NSMutableArray<NSNumber *> *scaledCiphertext = [NSMutableArray arrayWithArray:ciphertext];
-  for (int i = 0; i < n; i++) {
+  for (long i = 0; i < n; i++) {
     scaledCiphertext[i] = [NSNumber numberWithLong:[ciphertext[i] longValue] * factor];
   }
   NSLog(@"Ciphertext: %@", [scaledCiphertext componentsJoinedByString:@" "]);
@@ -100,7 +101,7 @@ int solve(NSArray<NSNumber *> *ciphertext, long factor, int rounds) {
   NSArray<NSNumber *>* plaintext = scaledCiphertext;
 
   // Apply the permutation for every round
-  for (int i = 0; i < rounds; i++) {
+  for (long i = 0; i < rounds; i++) {
     mix(scaledCiphertext, n, &state);
     plaintext = permuted(scaledCiphertext, state.permutation);
     NSLog(@"Plaintext: %@", [plaintext componentsJoinedByString:@" "]);
@@ -108,8 +109,8 @@ int solve(NSArray<NSNumber *> *ciphertext, long factor, int rounds) {
 
   // Find the solution
   long solution = 0;
-  int zeroOffset = [state.permutation[zeroIndex] intValue];
-  for (int i = 1000; i <= 3000; i += 1000) {
+  long zeroOffset = [state.permutation[zeroIndex] longValue];
+  for (long i = 1000; i <= 3000; i += 1000) {
     solution += [plaintext[mod(zeroOffset + i, n)] longValue];
   }
 
@@ -120,8 +121,8 @@ int main(void) {
   @autoreleasepool {
     NSArray<NSNumber *> *ciphertext = readInput(@"resources/demo.txt");
 
-    NSLog(@"Part 1: %d", solve(ciphertext, 1, 1));
-    NSLog(@"Part 2: %d", solve(ciphertext, 811589153, 10));
+    NSLog(@"Part 1: %ld", solve(ciphertext, 1, 1));
+    NSLog(@"Part 2: %ld", solve(ciphertext, 811589153, 10));
 
     return 0;
   }
