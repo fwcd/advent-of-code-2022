5d57a3a7bb62c5719ae6eb0998bad9b33dd55d34
fwcd <fwcdmail@gmail.com>
2022-12-23 18:48:58 +0100

    Parse day 16 input

--- day16/Program.fs
+++ day16/Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System.IO
+open System.Text.RegularExpressions
+
+type Valve =
+  { name: string
+    rate: int
+    neighbors: string list }
+
+let pattern = Regex(@"Valve (\w+) has flow rate=(\d+); tunnels lead to valves (.+)", RegexOptions.Compiled)
+
+let parseLine line =
+  match pattern.Match(line).Groups |> Seq.tail |> Seq.toList with
+    | [name; rate; neighbors] -> 
+      { name = name.Value
+        rate = rate.Value |> int
+        neighbors = neighbors.Value.Split(", ") |> Seq.toList }
+    | _ -> failwith ("Could not parse " + line)
+  
+
+let lines = File.ReadAllText("resources/demo.txt").Split("\n")
+let input = lines |> Seq.map parseLine
+
+printfn "Input: %A" input
