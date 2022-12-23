39d21659de8ca115f8f302ad9edc4becc8655f4c
fwcd <fwcdmail@gmail.com>
2022-12-23 23:52:17 +0100

    Only require edges (not nodes) to not be visited twice

--- day16/Program.fs
+++ day16/Program.fs
@@ -10,18 +10,22 @@ type Step =
   { name : string
     decision : int }
 
-let rec dfs (name : string) (graph : Map<string, Valve>) (visited : Set<string>) (remainingTime : int) : (int * Step list) =
-  if remainingTime > 0 && not (visited |> Set.contains name) then
+let rec dfs (name : string) (graph : Map<string, Valve>) (visited : Set<string * string>) (remainingTime : int) : (int * Step list) =
+  if remainingTime > 0 then
     let valve = Map.find name graph
-    let visited' = visited |> Set.add name
-    [0; 1]
-      |> Seq.collect (fun decision ->
-        let remainingTime' = remainingTime - decision - 1
-        let flow = decision * valve.rate * remainingTime
-        valve.neighbors
-          |> Seq.map (fun n ->
-            let (subFlow, subSteps) = dfs n graph visited' remainingTime'
-            flow + subFlow, { name = name; decision = decision } :: subSteps))
+    let candidates =
+      [0; 1]
+        |> Seq.collect (fun decision ->
+          let remainingTime' = remainingTime - decision - 1
+          let flow = decision * valve.rate * remainingTime
+          valve.neighbors
+            |> Seq.filter (fun n -> not (visited |> Set.contains (name, n)))
+            |> Seq.map (fun n ->
+              let visited' = visited |> Set.add (name, n)
+              let (subFlow, subSteps) = dfs n graph visited' remainingTime'
+              flow + subFlow, { name = name; decision = decision } :: subSteps))
+        |> Seq.toList
+    ((0, []) :: candidates)
       |> Seq.maxBy fst
   else
     0, []
@@ -38,7 +42,7 @@ let parseLine (line : string) : Valve option =
     | _ -> None
   
 let graph =
-  File.ReadAllText("resources/demo.txt").Split("\n")
+  File.ReadAllText("resources/mini.txt").Split("\n")
     |> Seq.choose parseLine 
     |> Seq.fold (fun m v -> Map.add v.name v m) Map.empty
 
