f62f790572e7a1b1679bc3467c01288b6aa24039
fwcd <fwcdmail@gmail.com>
2022-12-24 23:08:26 +0100

    Fix cube tesseleration for day 18's generated STL files

--- day18/test-implementation/day18.py
+++ day18/test-implementation/day18.py
@@ -77,28 +77,37 @@ def dfs_exterior_area(points: set[Point]) -> int:
     print(f'DFS Iterations: {iterations} (visited {len(visited)})')
     return exterior_sides
 
-def triangle_to_stl(points: list[Point], normal: np.ndarray) -> str:
+def triangle_to_stl(vertices: list[Point], normal: np.ndarray) -> str:
+    # Check if winding order matches, otherwise we'll have to invert it
+    inferred_normal = np.cross(vertices[1].values - vertices[0].values, vertices[2].values - vertices[0].values)
+    if np.dot(normal, inferred_normal) < 0:
+        vertices = vertices[::-1]
+
     return '\n'.join([
         f"facet normal {' '.join(map(str, normal))}",
         'outer loop',
-        *[f"vertex {point}" for point in points],
+        *[f'vertex {vertex}' for vertex in vertices],
         'endloop',
         'endfacet',
     ])
 
-def quad_to_stl(points: list[Point], normal: np.ndarray) -> str:
+def quad_to_stl(vertices: list[Point], normal: np.ndarray) -> str:
     return '\n'.join([
-        triangle_to_stl(points[:3], normal),
-        triangle_to_stl(points[1:], normal),
+        triangle_to_stl(vertices[:3], normal),
+        triangle_to_stl(vertices[2:] + [vertices[0]], normal),
     ])
 
-def point_to_face(point: Point, free_axes: list[int]) -> Iterable[Point]:
+def point_to_face(point: Point, free_axes: list[int]) -> list[Point]:
     if free_axes:
         axis = free_axes[0]
-        for d in [0, 1]:
-            yield from point_to_face(point.with_axis(axis, point[axis] + d), free_axes[1:])
+        other_axes = free_axes[1:]
+        return [
+            # We need to revert the second array to get a graycode-style, clockwise traversal of the face's vertices
+            *point_to_face(point.with_axis(axis, point[axis]), other_axes),
+            *point_to_face(point.with_axis(axis, point[axis] + 1), other_axes)[::-1]
+        ]
     else:
-        yield point
+        return [point]
 
 def point_to_cube_faces(point: Point) -> Iterable[tuple[list[Point], np.ndarray]]:
     axes = list(range(len(point.values)))
@@ -106,11 +115,11 @@ def point_to_cube_faces(point: Point) -> Iterable[tuple[list[Point], np.ndarray]
         for axis in axes:
             face_point = point.with_axis(axis, point[axis] + d)
             free_axes = axes[:axis] + axes[axis + 1:]
-            normal = face_point.values - point.values
-            yield (list(point_to_face(face_point, free_axes)), normal)
+            normal = np.eye(3)[axis] * (2 * d - 1)
+            vertices = point_to_face(face_point, free_axes)
+            yield (vertices, normal)
 
 def point_to_stl_cube(point: Point) -> str:
-    size = 1
     return '\n'.join([
         quad_to_stl(face, normal) for face, normal in point_to_cube_faces(point)
     ])
