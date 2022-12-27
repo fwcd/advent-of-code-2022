208f44a9c1c78ee2ed4a682a5638cfea62c1b469
fwcd <fwcdmail@gmail.com>
2022-12-27 19:19:43 +0100

    Parse blueprints and robots

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,3 +1,58 @@
+use std::{fs, collections::HashMap, str::FromStr};
+
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+#[derive(Debug, Clone)]
+struct Robot {
+    name: String,
+    costs: HashMap<String, usize>,
+}
+
+#[derive(Debug, Clone)]
+struct Blueprint {
+    robots: Vec<Robot>,
+}
+
+impl FromStr for Robot {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        const PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"Each (?P<name>\w+) robot costs (?P<raw_costs>.+)").unwrap());
+        const COST_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\d+) (\w+)").unwrap());
+
+        let captures = PATTERN.captures(s).ok_or_else(|| format!("Could not parse '{}'", s))?;
+        let name = captures.name("name").unwrap().as_str().to_owned();
+        let raw_costs = captures.name("raw_costs").unwrap();
+        let costs = COST_PATTERN.captures_iter(raw_costs.as_str())
+            .map(|c| (c[2].to_owned(), c[1].parse().unwrap()))
+            .collect::<HashMap<String, usize>>();
+
+        Ok(Robot { name, costs })
+    }
+}
+
+impl FromStr for Blueprint {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let robots = s.split(":")
+            .last().unwrap()
+            .split(".")
+            .filter_map(|r| r.parse().ok())
+            .collect::<Vec<_>>();
+        Ok(Blueprint { robots })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let blueprints = fs::read_to_string("resources/demo.txt").unwrap()
+        .split('\n')
+        .filter(|l| !l.is_empty())
+        .filter_map(|l| l.parse().ok())
+        .collect::<Vec<Blueprint>>();
+    
+    for blueprint in blueprints {
+        println!("{:#?}", blueprint);
+    }
 }
