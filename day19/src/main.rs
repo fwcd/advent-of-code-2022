8765501aff344fb96b50b18e82582650883e15e4
fwcd <fwcdmail@gmail.com>
2022-12-28 04:02:10 +0100

    Add first implementation of day 19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,7 +1,9 @@
-use std::{fs, collections::HashMap, str::FromStr};
+use std::{fs, collections::HashMap, str::FromStr, iter};
 
 use once_cell::sync::Lazy;
 use regex::Regex;
+
+// TODO: Use (fixed size) structs (e.g. `Materials`) instead of `HashMap`s?
 
 #[derive(Debug, Clone)]
 struct Robot {
@@ -12,6 +14,12 @@ struct Robot {
 #[derive(Debug, Clone)]
 struct Blueprint {
     robots: Vec<Robot>,
+}
+
+#[derive(Debug, Clone)]
+struct State {
+    robots: HashMap<String, usize>,
+    materials: HashMap<String, usize>,
 }
 
 impl FromStr for Robot {
@@ -45,6 +53,72 @@ impl FromStr for Blueprint {
     }
 }
 
+impl State {
+    fn new() -> Self {
+        Self {
+            robots: [("ore".to_owned(), 1)].into(),
+            materials: [].into(),
+        }
+    }
+
+    fn geodes(&self) -> usize {
+        *self.materials.get("geode").unwrap_or(&0)
+    }
+
+    // TODO: Fixed size instead of `Vec`s?
+
+    fn step(&mut self) {
+        for (material, count) in self.robots.iter() {
+            let current = *self.materials.get(material).unwrap_or(&0);
+            self.materials.insert(material.clone(), current + count);
+        }
+    }
+
+    fn spend(&mut self, deltas: &HashMap<String, usize>) {
+        for (material, delta) in deltas.iter() {
+            let current = *self.materials.get(material).unwrap_or(&0);
+            self.materials.insert(material.clone(), current - delta);
+        }
+    }
+
+    fn next(&self, robot: Option<&Robot>) -> Self {
+        let mut next = self.clone();
+        if let Some(robot) = robot {
+            next.spend(&robot.costs);
+        }
+        next.step();
+        if let Some(robot) = robot {
+            let current = *next.robots.get(&robot.name).unwrap_or(&0);
+            next.robots.insert(robot.name.clone(), current + 1);
+        }
+        next
+    }
+
+    fn childs<'a>(&'a self, blueprint: &'a Blueprint) -> impl Iterator<Item = Self> + 'a {
+        iter::once(self.next(None)).chain(
+            blueprint.robots.iter()
+                .map(|r| self.next(Some(r)))
+        )
+    }
+
+    fn dfs_geodes(&self, blueprint: &Blueprint, remaining_minutes: usize) -> usize {
+        if remaining_minutes == 0 {
+            self.geodes()
+        } else {
+            self.childs(blueprint)
+                .map(|c| c.dfs_geodes(blueprint, remaining_minutes - 1))
+                .max()
+                .unwrap_or(0)
+        }
+    }
+}
+
+impl Blueprint {
+    fn quality_level(&self, remaining_minutes: usize) -> usize {
+        State::new().dfs_geodes(self, remaining_minutes)
+    }
+}
+
 fn main() {
     let blueprints = fs::read_to_string("resources/demo.txt").unwrap()
         .split('\n')
@@ -52,7 +126,9 @@ fn main() {
         .filter_map(|l| l.parse().ok())
         .collect::<Vec<Blueprint>>();
     
-    for blueprint in blueprints {
-        println!("{:#?}", blueprint);
-    }
+    let part1 = blueprints.iter().enumerate()
+        .map(|(i, b)| (i + 1) * b.quality_level(24))
+        .sum::<usize>();
+
+    println!("Part 1: {}", part1);
 }
