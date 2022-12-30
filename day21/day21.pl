7784765af069d210fe3dd5d969b26a65e039f209
fwcd <fwcdmail@gmail.com>
2022-12-30 23:37:45 +0100

    Make variable naming more consistent

--- day21/day21.pl
+++ day21/day21.pl
@@ -6,10 +6,10 @@ lookup_expr(Var, [eqn(Var, Expr)|_], Expr) :- !.
 lookup_expr(Var, [_|Eqns], Expr) :- lookup_expr(Var, Eqns, Expr).
 
 build_tree(Root, Eqns, const(X)) :- lookup_expr(Root, Eqns, const(X)), !.
-build_tree(Root, Eqns, bin_op(LhsNode, Op, RhsNode)) :-
-  lookup_expr(Root, Eqns, bin_op(Lhs, Op, Rhs)),
-  build_tree(Lhs, Eqns, LhsNode),
-  build_tree(Rhs, Eqns, RhsNode).
+build_tree(Root, Eqns, bin_op(Lhs, Op, Rhs)) :-
+  lookup_expr(Root, Eqns, bin_op(LhsVar, Op, RhsVar)),
+  build_tree(LhsVar, Eqns, Lhs),
+  build_tree(RhsVar, Eqns, Rhs).
 
 eval(X, plus, Y, Z) :- Z is X + Y, !.
 eval(X, minus, Y, Z) :- Z is X - Y, !.
@@ -27,7 +27,7 @@ dcg_eqns([E|Es]) --> dcg_eqn(E), dcg_eqns(Es).
 dcg_eqn(eqn(Res, Expr)) --> dcg_var(Res), ": ", dcg_expr(Expr), !.
 
 dcg_expr(const(X)) --> number(X), eol, !.
-dcg_expr(bin_op(Lhs, Op, Rhs)) --> dcg_var(Lhs), " ", dcg_op(Op), " ", dcg_var(Rhs), eol, !.
+dcg_expr(bin_op(LhsVar, Op, RhsVar)) --> dcg_var(LhsVar), " ", dcg_op(Op), " ", dcg_var(RhsVar), eol, !.
 
 dcg_var(Id) --> string(IdCs),
   { atom_codes(Id, IdCs) }.
