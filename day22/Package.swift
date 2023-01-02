cd44c436f475e4696c7f76eea77734c357798828
fwcd <fwcdmail@gmail.com>
2023-01-02 09:23:53 +0100

    Parse day 22 input

--- day22/Package.swift
+++ day22/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "day22",
+    platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .executable(
@@ -21,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Day22",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [.unsafeFlags(["-enable-bare-slash-regex"])]
         ),
     ]
 )
--- day22/Sources/Day22/main.swift
+++ day22/Sources/Day22/main.swift
@@ -1,6 +1,29 @@
-public struct day22 {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+enum Direction: String {
+  case left = "L"
+  case right = "R"
 }
+
+enum Instruction {
+  case tiles(Int)
+  case turn(Direction)
+}
+
+let url = URL(filePath: "Resources/demo.txt")
+let input = String(data: try Data(contentsOf: url), encoding: .utf8)!
+let rawParts = input.split(separator: "\n\n")
+
+let map = Array(rawParts[0].split(separator: "\n").map(Array.init))
+let instructions = rawParts[1].matches(of: /(?<tiles>\d+)|(?<dir>[LR])/).map { match -> Instruction in
+  let output = match.output
+  if let tiles = output.tiles {
+    return .tiles(Int(tiles)!)
+  } else if let dir = output.dir {
+    return .turn(Direction(rawValue: String(dir))!)
+  } else {
+    fatalError("Unreachable")
+  }
+}
+
+print(instructions)
