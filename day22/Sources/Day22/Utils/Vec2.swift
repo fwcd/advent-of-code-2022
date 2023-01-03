d861e68b68e15fcc4963410930e9c107d2ba0e35
fwcd <fwcdmail@gmail.com>
2023-01-03 19:53:49 +0100

    Move vec initializers back into extensions
    To avoid deleting the implicit struct initializers

--- day22/Sources/Day22/Utils/Vec2.swift
+++ day22/Sources/Day22/Utils/Vec2.swift
@@ -6,19 +6,6 @@ struct Vec2: Hashable, CustomStringConvertible {
 
   static var zero = Self()
 
-  init(fromYz vec3: Vec3) {
-    self.init(x: vec3.y, y: vec3.z)
-  }
-
-  init(_ direction: Direction) {
-    switch direction {
-    case .left:  self.init(x: -1, y:  0)
-    case .up:    self.init(x:  0, y: -1)
-    case .right: self.init(x:  1, y:  0)
-    case .down:  self.init(x:  0, y:  1)
-    }
-  }
-
   static func +(lhs: Self, rhs: Self) -> Self {
     Self(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
   }
@@ -33,5 +20,20 @@ struct Vec2: Hashable, CustomStringConvertible {
 
   static func /(lhs: Self, rhs: Int) -> Self {
     Self(x: lhs.x / rhs, y: lhs.y / rhs)
+  }
+}
+
+extension Vec2 {
+  init(_ direction: Direction) {
+    switch direction {
+    case .left:  self.init(x: -1, y:  0)
+    case .up:    self.init(x:  0, y: -1)
+    case .right: self.init(x:  1, y:  0)
+    case .down:  self.init(x:  0, y:  1)
+    }
+  }
+
+  init(fromYz vec3: Vec3) {
+    self.init(x: vec3.y, y: vec3.z)
   }
 }
--- day22/Sources/Day22/Utils/Vec3.swift
+++ day22/Sources/Day22/Utils/Vec3.swift
@@ -7,11 +7,13 @@ struct Vec3: Hashable, CustomStringConvertible {
 
   static var zero = Self()
 
-  init(yz vec2: Vec2) {
-    self.init(y: vec2.x, z: vec2.y)
-  }
-
   func dot(_ rhs: Self) -> Int {
     x * rhs.x + y * rhs.y + z * rhs.z
+  }
+}
+
+extension Vec3 {
+  init(yz vec2: Vec2) {
+    self.init(y: vec2.x, z: vec2.y)
   }
 }
