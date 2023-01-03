af50455b2cab6b92c9800d78f3511f2b8b2e0832
fwcd <fwcdmail@gmail.com>
2023-01-03 18:09:01 +0100

    Add test for cube map

--- day22/Tests/Day22Tests/BoardTests.swift
+++ day22/Tests/Day22Tests/BoardTests.swift
@@ -17,5 +17,14 @@ class BoardTests: XCTestCase {
               .#......
               ......#.
       """, cubeSize: 4)
+
+    XCTAssertEqual(fields.cubeMap, [
+      Vec2(x: 2, y: 0): Mat3(e0: Vec3(x: 1, y: 0, z: 0), e1: Vec3(x: 0, y: 1, z: 0), e2: Vec3(x: 0, y: 0, z: 1)), // top
+      Vec2(x: 2, y: 1): Mat3(e0: Vec3(x: 0, y: 0, z: 1), e1: Vec3(x: 0, y: 1, z: 0), e2: Vec3(x: -1, y: 0, z: 0)), // front
+      Vec2(x: 2, y: 2): Mat3(e0: Vec3(x: -1, y: 0, z: 0), e1: Vec3(x: 0, y: 1, z: 0), e2: Vec3(x: 0, y: 0, z: -1)), // bottom
+      Vec2(x: 1, y: 1): Mat3(e0: Vec3(x: 0, y: -1, z: 0), e1: Vec3(x: 0, y: 0, z: 1), e2: Vec3(x: -1, y: 0, z: 0)), // left
+      Vec2(x: 3, y: 2): Mat3(e0: Vec3(x: 0, y: 1, z: 0), e1: Vec3(x: 1, y: 0, z: 0), e2: Vec3(x: 0, y: 0, z: -1)), // right
+      Vec2(x: 0, y: 1): Mat3(e0: Vec3(x: 0, y: 0, z: -1), e1: Vec3(x: 0, y: -1, z: 0), e2: Vec3(x: -1, y: 0, z: 0)), // back
+    ])
   }
 }
