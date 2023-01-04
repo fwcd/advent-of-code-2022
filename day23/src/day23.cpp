df9f803f2146fcc021b01a4a8f4181066838e9f3
fwcd <fwcdmail@gmail.com>
2023-01-04 20:52:30 +0100

    Read day 23 input

--- day23/src/day23.cpp
+++ day23/src/day23.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> readInput() {
+  std::ifstream stream;
+  stream.open("resources/demo.txt");
+  std::vector<std::string> lines;
+  std::string line;
+  while (stream.good()) {
+    stream >> line;
+    lines.push_back(line);
+  }
+  return lines;
+}
 
 int main() {
-  std::cout << "Hello world" << std::endl;
+  const std::vector<std::string> input {readInput()};
+  for (const std::string &line : input) {
+    std::cout << line << std::endl;
+  }
   return 0;
 }
