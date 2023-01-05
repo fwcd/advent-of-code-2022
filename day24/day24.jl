5672f207f1f8deb52974344cbc772a7639d9c137
fwcd <fwcdmail@gmail.com>
2023-01-05 02:46:22 +0100

    Factor out positions

--- day24/day24.jl
+++ day24/day24.jl
@@ -11,6 +11,14 @@ struct State
     size::Vec2
 end
 
+function positions(width::Int, height::Int)
+    Iterators.map(t -> Vec2(t[1], t[2]), Iterators.product(1:width, 1:height))
+end
+
+function positions(state::State)
+    positions(state.size.x, state.size.y)
+end
+
 function parse_dir(raw::Char)
     if raw == '>'
         Vec2(1, 0)
@@ -29,8 +37,7 @@ function parse_input(lines::Vector{String})
     height = length(lines) - 2
     width = length(lines[1]) - 2
     size = Vec2(width, height)
-    positions = Iterators.map(t -> Vec2(t[1], t[2]), Iterators.product(1:width, 1:height))
-    parsed = Iterators.map(p -> (p, parse_dir(lines[p.y + 1][p.x + 1])), positions)
+    parsed = Iterators.map(p -> (p, parse_dir(lines[p.y + 1][p.x + 1])), positions(width, height))
     blizzards = Dict(Iterators.filter(t -> !isnothing(t[2]), parsed))
     return State(nothing, blizzards, size)
 end
