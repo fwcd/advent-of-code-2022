9d1e3155f6ba3651704d2bd792bbcc945b9e4fc4
fwcd <fwcdmail@gmail.com>
2023-01-09 03:59:43 +0100

    Compute 25.1 SNAFU

--- day25/day25.py
+++ day25/day25.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-def parse_snafu_digit(raw: str):
+def parse_snafu_digit(raw: str) -> int:
     return {'=': -2, '-': -1}.get(raw) or int(raw)
 
-def parse_snafu(raw: str):
+def parse_snafu(raw: str) -> int:
     result = 0
     factor = 1
     for raw_digit in reversed(raw):
@@ -11,8 +11,20 @@ def parse_snafu(raw: str):
         factor *= 5
     return result
 
-with open('resources/demo.txt', 'r') as f:
+def snafu(n: int) -> str:
+    digits = []
+    base = 5
+    offset = 2
+    while n != 0:
+        n += offset
+        d = n % base - offset
+        digits.append({-2: '=', -1: '-'}.get(d) or str(d))
+        n //= base
+    return ''.join(reversed(digits))
+
+with open('resources/input.txt', 'r') as f:
     lines = [l.strip() for l in f.readlines() if l.strip()]
 
 part1 = sum(parse_snafu(raw) for raw in lines)
 print(f'Part 1 (decimal): {part1}')
+print(f'Part 1 (SNAFU): {snafu(part1)}')
